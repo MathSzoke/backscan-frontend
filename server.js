d644187699f94b9e38fbb8b2bfa861db80df2cfa
Matheus Szoke <matheusszoke@gmail.com>
2025-04-15 19:38:07 -0300

    feat: renaming name

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const TELEGRAM_BOT_TOKEN = "BOT-TOKEN"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "CHAT-TOKEN"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+const TELEGRAM_BOT_TOKEN = "8127314666:AAEPtxSUC77AS8zpdFahc2VQMVXaQlhk5hc"; // Substitua pelo token do seu bot
+const TELEGRAM_CHAT_ID = "7185138030"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
