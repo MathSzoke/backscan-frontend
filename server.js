88ab10bdec9467370d488fadc77bef968993b50c
Matheus Szoke <matheusszoke@gmail.com>
2025-04-15 19:53:11 -0300

    feat: adding environment variable

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const TELEGRAM_BOT_TOKEN = "8127314666:AAEPtxSUC77AS8zpdFahc2VQMVXaQlhk5hc"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "7185138030"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
